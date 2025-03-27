c6e4408d59c8b4538bb3295219bc8fd8bdb81fb1
jdifek <givotkov11@gmail.com>
2025-03-27 19:27:17 +0200

    wqwq

--- app/[locale]/auth/registerSuccess/page.tsx
+++ app/[locale]/auth/registerSuccess/page.tsx
@@ -1,31 +1,26 @@
-"use client";
+'use client'
 
-import { useTranslations } from "next-intl";
-import Image from "next/image";
-import { usePathname } from "next/navigation";
-import { useRouter } from "next/navigation"; // ✅ Правильно
-import { useEffect } from "react";
-
+import { useTranslations } from 'next-intl'
+import Image from 'next/image'
+import { useSearchParams } from 'next/navigation'
 const RegisterSuccess = () => {
-  const router = useRouter();
-  const pathname = usePathname();
-  const locale = pathname.split("/")[1] || "ru";
-  const tAuth = useTranslations("Auth");
-  useEffect(() => {
-    setTimeout(() => router.push(`/${locale}/auth/login`), 3000);
-  }, []);
-  return (
-    <div className="rounded-[12px] px-[20px] pt-4 py-[108px] w-full flex-col items-center flex mb-3 justify-center mt-9 h-[443px] bg-[#f3f4f7]">
-      <Image src="/Pic.svg" height={150} width={150} alt="pic"></Image>
+	const tAuth = useTranslations('Auth')
+	const searchParams = useSearchParams()
+	const email = searchParams.get('email') || 'mail@gmail.com'
 
-      <p className="text-center text-[14px] font-normal leading-[93%] text-black mb-3 mt-5">
-        {tAuth("thanksForRegistering")}
-      </p>
-      <p className="text-center text-[13px] font-light leading-[92%] text-[#383838]">
-        {tAuth("linkToActivate")} mail@gmail.com
-      </p>
-    </div>
-  );
-};
+	return (
+		<div className='rounded-[12px] px-[20px] pt-4 py-[108px] w-full flex-col items-center flex mb-3 justify-center mt-9 h-[443px] bg-[#f3f4f7]'>
+			<Image src='/Pic.svg' height={150} width={150} alt='pic'></Image>
 
-export default RegisterSuccess;
+			<p className='text-center text-[14px] font-normal leading-[93%] text-black mb-3 mt-5'>
+				{tAuth('thanksForRegistering')}
+			</p>
+			<p className='text-center text-[13px] font-light leading-[92%] text-[#383838]'>
+				{tAuth('linkToActivate')}{' '}
+				<span className='font-medium'>{decodeURIComponent(email)}</span>
+			</p>
+		</div>
+	)
+}
+
+export default RegisterSuccess
